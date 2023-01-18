3cb8b21b31afb09158de51dd7dd13ab45dbd0d51
Benjamin Ly <benjaminly702@gmail.com>
2023-01-18 13:18:49 -0800

    Basic main page ui created

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Button from '@mui/material/Button';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
 import { Stack, TextField, Typography } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/navbar/navbar';
+import { Mainpage } from './components/mainpage/mainpage';
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
 const client = createDockerDesktopClient();
@@ -14,46 +17,37 @@ export function App() {
   const [response, setResponse] = React.useState<string>();
   const ddClient = useDockerDesktopClient();
 
+  useEffect(() => {
+    const callContainers = async () => {
+      const containers = await ddClient.docker.listContainers();
+      setResponse(JSON.stringify(containers));
+    };
+    callContainers().catch(console.error);
+  });
   const fetchAndDisplayResponse = async () => {
-    console.log('Hello! You pressed the button :)')
+    console.log('Hello! You pressed the button :)');
     const result = await ddClient.extension.vm?.service?.get('/hello');
 
     //This line gets the containers from the ddClient
     const containers = await ddClient.docker.listContainers();
     console.log('Running containers are:', containers);
 
-    setResponse(JSON.stringify(result));
+    setResponse(JSON.stringify(containers));
+    // console.log(typeof response);
   };
 
   return (
     <>
-      {/* <Testing /> */}
-      <Typography variant="h3">Docker extension demo</Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        This is a basic page rendered with MUI, using Docker's theme. Read the
-        MUI documentation to learn more. Using MUI in a conventional way and
-        avoiding custom styling will help make sure your extension continues to
-        look great as Docker's theme evolves.
-      </Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        Pressing the below button will trigger a request to the backend. Its
-        response will appear in the textarea.
-      </Typography>
-      <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
-        <Button variant="contained" onClick={fetchAndDisplayResponse}>
-          Call backend
-        </Button>
-
-        <TextField
-          label="Backend response"
-          sx={{ width: 480 }}
-          disabled
-          multiline
-          variant="outlined"
-          minRows={5}
-          value={response ?? ''}
-        />
-      </Stack>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Mainpage containers={response} />} />
+          {/* <Route path='/container/:id'  /> */}
+        </Routes>
+      </Router>
+      {/* <Button variant="contained" onClick={fetchAndDisplayResponse}>
+        Call backend
+      </Button> */}
     </>
   );
 }
