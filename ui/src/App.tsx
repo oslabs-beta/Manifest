69073490fb0e12c954ad295fec3967d0cbf3a997
EliotOrando <eorando@gmail.com>
2023-01-21 13:55:32 -0800

    should be a working version

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -12,11 +12,6 @@ import ContainerContext from './container-context';
 
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
-const client = createDockerDesktopClient();
-
-function useDockerDesktopClient() {
-  return client;
-}
 
 export function App() {
 
@@ -29,13 +24,25 @@ export function App() {
       .exec('stats', ['--no-stream', '--no-trunc', '--format', '"{{json .}}"'])
       .then((res) => res.parseJsonLines());
     setDataStore(stats);
+    
     setContainersLoaded(true);
   };
 
   //will run once when container is loaded
-  useEffect(() => {
-    updateContainerData();
+  useEffect(() => { 
+    updateContainerData(); 
   }, [])
+
+  //this will run whenever the dataStore Changes
+  useEffect(() => {
+    setTimeout(async () => {
+      console.log('dataStore changed Deteceted, checking the store');
+      const stats = await ddClient.docker.cli
+      .exec('stats', ['--no-stream', '--no-trunc', '--format', '"{{json .}}"'])
+      .then((res) => res.parseJsonLines());
+      console.log('we got data back')
+    }, 1000);
+  }, [dataStore])
 
   return (
     <>
